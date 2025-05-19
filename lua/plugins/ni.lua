7193888ce3709130841e804dd930944341f1de7c
nicolas <nicolas.nkruge@gmaila.com>
2025-05-19 06:28:14 -0300

    :fire: (fire) flame
    fuego
    
    BREAKING CHANGE: agni

---
+++ lua/plugins/ni.lua
@@ -0,0 +1,17 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+-- if true then return {} end
+
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      table.insert(opts.ensure_installed, "js-debug-adapter")
+    end
+  }
