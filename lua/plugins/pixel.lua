15f086f8fcda22c14d8ceb8da962605114ca0e62
nicolas <nicolas.nkruge@gmaila.com>
2025-05-25 12:22:16 -0300

    :fire:

---
+++ lua/plugins/pixel.lua
@@ -0,0 +1,11 @@
+-- ~/.config/nvim/lua/plugins/myplugin.lua
+
+return {
+  "nicolaskruger/pxt", -- use any plugin or dummy one
+  enabled = true,
+  config = function()
+    vim.api.nvim_create_user_command("HelloWorld", function()
+      print("Hello from LazyVim plugin!")
+    end, {})
+  end,
+}
